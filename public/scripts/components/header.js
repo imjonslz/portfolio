c23dfd95afe5fc9cd16b3ecb41c88711361eca4d
imjonslz <im.jonslz@gmail.com>
2025-07-09 23:45:12 -0300

    refactor header component and fix images performance

--- public/scripts/components/header.js
+++ public/scripts/components/header.js
@@ -3,6 +3,8 @@ const toggle = document.getElementById("menu-toggle");
 const menu = document.getElementById("main-menu");
 const iconMenu = document.getElementById("iconMenu");
 const iconClose = document.getElementById("iconClose");
+const wrapperLogo = document.getElementById("wrapperLogo");
+
 if (toggle && menu && iconMenu && iconClose) {
     toggle.addEventListener("click", () => {
         // Toggle the 'open' class on the menu and update the aria-expanded attribute
@@ -11,5 +13,7 @@ if (toggle && menu && iconMenu && iconClose) {
         // Toggle the display of the menu and icons
         iconMenu.style.display = isOpen ? "none" : "flex";
         iconClose.style.display = isOpen ? "flex" : "none";
+
+        wrapperLogo.style.zIndex = isOpen ? "1000" : "1";
     });
 }
--- src/scripts/components/header.ts
+++ src/scripts/components/header.ts
@@ -2,8 +2,9 @@ const toggle = document.getElementById("menu-toggle") as HTMLButtonElement | nul
 const menu = document.getElementById("main-menu") as HTMLUListElement | null;
 const iconMenu = document.getElementById("iconMenu") as HTMLElement | null;
 const iconClose = document.getElementById("iconClose") as HTMLElement | null;
+const wrapperLogo = document.getElementById("wrapperLogo") as HTMLAnchorElement | null;
 
-if (toggle && menu && iconMenu && iconClose) {
+if (toggle && menu && iconMenu && iconClose && wrapperLogo) {
   toggle.addEventListener("click", () => {
     // Toggle the 'open' class on the menu and update the aria-expanded attribute
     const isOpen = menu.classList.toggle("open");
@@ -12,5 +13,7 @@ if (toggle && menu && iconMenu && iconClose) {
     // Toggle the display of the menu and icons
     iconMenu.style.display = isOpen ? "none" : "flex";
     iconClose.style.display = isOpen ? "flex" : "none";
+
+    wrapperLogo.style.zIndex = isOpen ? "1000" : "1";
   });
 }
